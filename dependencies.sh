a4299367f96b83cceb19865872c0b0c52d4f865c
Billy Zhou <summerstorm@users.noreply.github.com>
2016-10-15 00:10:28 -0700

    Removed the python3-yaml dependency from dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo apt-get install python3-pip python3-yaml python3-gi libappindicator3-dev
+sudo apt-get install python3-pip python3-gi libappindicator3-dev
 sudo pip3 install https://github.com/wistful/pympris/archive/master.zip
